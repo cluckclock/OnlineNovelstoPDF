89f28371b6661fd96cd30e3e0679e5ed6b0eb86d
cluckclock <dr.who89@hotmail.com>
2018-06-12 21:28:56 +0300

    add automatic setup for ubuntu

---
+++ debian-setup.sh
@@ -0,0 +1,8 @@
+echo Setting up Pip for Python3
+sudo apt-get install python3-pip
+echo Installing Mechanicalsoup
+pip3 install mechanicalsoup
+echo Installing pypdf
+pip3 install pypdf
+echo done!
+echo To run software, type: python3 main.py
